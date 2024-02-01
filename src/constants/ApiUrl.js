2780d6817496f9bfc07f640a3c0010970b1d5e61
PogleJeong <jes4958@yonsei.ac.kr>
2024-02-01 15:12:54 +0900

    URL PATH 추가

--- src/constants/ApiUrl.js
+++ src/constants/ApiUrl.js
@@ -1,3 +1,4 @@
+// account
 export const LOGIN_BY_KAKAO = "/kakao/login";
 export const LOGIN_BY_NAVER = "/naver/login";
 export const FIND_ID = "/find/id";
@@ -5,4 +6,10 @@ export const FIND_PASSWORD = "/find/password";
 export const CHECK_ID = "/check/id";
 export const CHECK_EMAIL = "/check/email";
 
+// pet
 export const ADD_PET = "/add/pet";
+export const GET_USER_PET = "/get/pet";
+
+// user
+export const FOLLOW = "/user/follow";
+export const UNFOLLOW = "/user/unfollow"
--- src/constants/UrlPath.js
+++ src/constants/UrlPath.js
@@ -1,3 +1,4 @@
+// member, pet 관련
 export const LOGIN = "/login";
 export const REGISTER = "/register";
 export const PET_ADD = "/register/choice";
@@ -6,4 +7,8 @@ export const FIND_ACCOUNT = "/account/find";
 export const FIND_ACCOUNT_ID_RESULT = "/account/result/id";
 export const FIND_ACCOUNT_PASSWORD_RESULT = "/account/result/password";
 
-export const HOME = "/home"
+export const MARKET = "/market";
+export const MARKET_SELL = "/market/sell";
+export const MARKET_FREE = "/market/free";
+
+export const HOME = "/"
