520279ede03c733444756e97cd84a801fbad41f9
PogleJeong <jes4958@yonsei.ac.kr>
2024-02-01 15:05:48 +0900

    reset css, router 추가

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// provider
+import { CookiesProvider } from 'react-cookie';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+// css - bootstrap, reset css
+import { createGlobalStyle } from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reset from './index.css'; // css reset
 
-import './index.css';
+// inner import
 import reportWebVitals from './reportWebVitals';
 import Main from './common/Root';
-import Login from './router/Login';
+import Login from './router/account/Login';
 import NotFound from './error_page/404';
-import FindAccount from './router/FindAccount';
-import FindPassword from './router/FindPassword';
-import RegisterUser from './router/RegisterUser';
-import ChoiceToRegisterPet from './router/ChoiceToRegisterPet.';
-import RegisterPet from './router/RegisterPet';
-import FindId from './router/FindId';
+import FindAccount from './router/account/FindAccount';
+import RegisterUser from './router/account/RegisterUser';
+import ChoiceToRegisterPet from './router/account/ChoiceToRegisterPet.';
+import RegisterPet from './router/account/RegisterPet';
+import FindId from './router/account/FindId';
 import Root from './common/Root';
+import ChangePassword from './router/account/ChangePassword';
+import UserInfo from './router/user/UserInfo';
+import MarketHome from './router/market/market-home/MarketHome';
+import MarketFreeList from './router/market/market-home/component/MarketFreeList';
+import MarketSellList from './router/market/market-home/component/MarketSellList';
+import WriteMarketBorder from './router/market/WriteMarketBorder';
+import UpdateMarketBorder from './router/market/UpdateMarketBorder';
+import ViewMarketBorder from './router/market/ViewMarketBorder';
 
+const globalStyle = createGlobalStyle`
+  ${reset}
+`
 /**
  * router 에서 children 은 parent 컴포넌트 안의 Outlet 에 배치할 컴포넌트를 정의하는 것이다.
  */
@@ -35,34 +52,68 @@ const router = createBrowserRouter([
         children: [
           {
             path: "find",
-            element: <FindAccount />
+            element: <FindAccount />,
           },
           {
             path: "result",
             children: [
               {
                 path: "id",
-                element: <FindId />
+                element: <FindId />,
               },
-              {
-                path: "password",
-                element: <FindPassword />
-              }
             ]
           },
         ]
       },
       {
         path: "register",
-        element: <RegisterUser />,
+        element: <RegisterUser />
       },
       {
         path: "register/choice",
-        element: <ChoiceToRegisterPet />
+        element: <ChoiceToRegisterPet />,
       }, 
       {
         path: "register/pet",
-        element: <RegisterPet />
+        element: <RegisterPet />,
+      },
+      {
+        path: "user/:id",
+        element: <UserInfo />,
+      },
+      {
+        path: "user/change-password",
+        element: <ChangePassword />
+      },
+      {
+        path: "user/update/profile",
+        element: <UserInfo />,
+      },
+      {
+        path: "market",
+        element: <MarketHome />,
+        children: [
+          {
+            path: "free",
+            element: <MarketFreeList />,
+          },
+          {
+            path: "sell",
+            element: <MarketSellList />,
+          }
+        ]
+      },
+      {
+        path: "market/write",
+        element: <WriteMarketBorder />,
+      },
+      {
+        path: "market/update/:id",
+        element: <UpdateMarketBorder />
+      },
+      {
+        path: "market/detail/:id",
+        element: <ViewMarketBorder />
       }
     ],
     errorElement: <NotFound />,
@@ -75,7 +126,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router}>
-      <Main />
+      <CookiesProvider>
+        <Main />
+      </CookiesProvider>
     </RouterProvider>
   </React.StrictMode>
 );
